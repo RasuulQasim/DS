33a33f6d73c7cead302bf548b6d2af6361a13f33
ghareeb <hi@gmail.com>
2023-12-28 16:57:16 +0300

    add containt to this is file

---
+++ main.java
@@ -0,0 +1,23 @@
+public class main {
+	
+	
+
+	public static void main( String[] args) {
+		
+		DoubleNode dNode=new DoubleNode(null, "A", null);
+		DoubleLinkedList dlist=new DoubleLinkedList(dNode);
+		dlist.addFirst(new DoubleNode(null, "B", null));
+		dlist.addFirst(new DoubleNode(null, "C", null));
+		dlist.addFirst(new DoubleNode(null, "D", null));
+		dlist.addFirst(new DoubleNode(null, "E", null));
+		dlist.addFirst(new DoubleNode(null, "F", null));
+		dlist.delete("n");
+		dlist.addLast(new DoubleNode(null, "X", null));
+		dlist.delete("B");
+		dlist.addFirst(new DoubleNode(null, "T", null));
+		
+		System.out.println(dlist.toString());
+
+	}
+
+}

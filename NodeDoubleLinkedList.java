efb2b71264d5de38c0321c425f50dc99366cfa20
ghareeb <hi@gmail.com>
2023-12-28 16:53:53 +0300

    add comtaint NodeDoubleLinkedList

---
+++ NodeDoubleLinkedList.java
@@ -0,0 +1,16 @@
+public class NodeDoubleLinkedList {
+	
+	Object data;
+	DoubleNode next;
+	DoubleNode previous;
+	
+	public DoubleNode(DoubleNode previous,Object data,DoubleNode next) {
+		this.previous=previous;
+		this.data=data;
+		this.next=next;
+	}
+	
+	
+	
+
+}
